7dc125c0b21c7f95df4bd134e314b96a30c3c4a5
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-05-12 02:41:41 +0000

    File version updated to 1.0.0.1.

--- TwainProxy/Properties/AssemblyInfo.cs
+++ TwainProxy/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shim application to use 32-bit TWAIN drivers.")]
+[assembly: AssemblyTitle("Shim application to use 32-bit TWAIN drivers from 64-bit Paint.NET.")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
