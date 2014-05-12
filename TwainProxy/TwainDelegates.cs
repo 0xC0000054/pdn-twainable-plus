bd31c474f7f903ec3f3cd4459b2f42fba4148e18
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-05-12 01:03:02 +0000

    Marked all delegates with UnmanagedFunctionPointer and SupressUnmanagedCodeSecurity.

--- TwainProxy/TwainDelegates.cs
+++ TwainProxy/TwainDelegates.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace TwainProxy
 {
     internal static class TwainDelegates
     {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
         internal delegate ResultCode TwainEvent(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Event item);
+                
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
         internal delegate ResultCode TwianParent(ref TwainStructs.Identity appId, IntPtr deviceId, DataGroup group, ushort argType, ushort msg, ref IntPtr parentHWnd);
-        internal delegate ResultCode TwainIdentity(ref TwainStructs.Identity appId, IntPtr deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Identity currentDevice);
-        internal delegate ResultCode TwainUI(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.UserInterface item);
-        internal delegate ResultCode TwainCapability(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Capability item);
-        internal delegate ResultCode TwianNativeTransfer(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref IntPtr hDib);
-        internal delegate ResultCode TwianPendingXfer(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.PendingXfers item);
-        internal delegate ResultCode TwainStatus(ref TwainStructs.Identity appId, IntPtr deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Status status);
 
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
+        internal delegate ResultCode TwainIdentity(ref TwainStructs.Identity appId, IntPtr deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Identity currentDevice);
+               
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
+        internal delegate ResultCode TwainUI(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.UserInterface item);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
+        internal delegate ResultCode TwainCapability(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Capability item);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
+        internal delegate ResultCode TwianNativeTransfer(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref IntPtr hDib);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
+        internal delegate ResultCode TwianPendingXfer(ref TwainStructs.Identity appId, ref TwainStructs.Identity deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.PendingXfers item);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall), SuppressUnmanagedCodeSecurity]
+        internal delegate ResultCode TwainStatus(ref TwainStructs.Identity appId, IntPtr deviceId, DataGroup group, ushort argType, ushort msg, ref TwainStructs.Status status);
     }
 }
