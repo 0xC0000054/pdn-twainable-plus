630ad9bea23f094a465c74dab57e3116c32ff31a
0xC0000054 <0xC0000054@users.noreply.github.com>
2018-01-03 05:19:51 -0700

    Bump copyright year

--- TwainProxy/TwainDelegates.cs
+++ TwainProxy/TwainDelegates.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-twainable-plus, an Effect plugin for
 // Paint.NET that imports images from TWAIN devices.
 //
-// Copyright (c) 2014, 2017 Nicholas Hayes
+// Copyright (c) 2014, 2017, 2018 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
